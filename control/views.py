62e31bb17b38c765d4b5df2cc932e32e8c63a04b
Patrick Shannon <pinaz993@gmail.com>
2021-05-13 12:59:07 -0400

    Reintegrated timer display client logic in the control interface. Fixed broken login redirection issue.

--- control/views.py
+++ control/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import logout_then_login
 
 
-@login_required
+@login_required(login_url='login/')
 def control(request):
     return render(request, 'control/control.html')
-
-
-def login(request):
-    if request.user.is_authenticated:
-        return redirect(control)
-    return render(request, 'registration/login.html')
